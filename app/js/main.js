1549de88b271f6274c47e6453b61aa14ca4f5cc9
AlexITHoot <alex.pylypchuk@ithoot.com>
2021-02-08 20:38:00 +0200

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,39 @@
+ // Menu button
+ (function() {
+    "use strict";
+    var toggles = document.querySelectorAll(".c-hamburger");
+    for (var i = toggles.length - 1; i >= 0; i--) {
+      var toggle = toggles[i];
+      toggleHandler(toggle);
+    };
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      });
+    }
+  })();
+  
+  ///Drop menu
+  $(function(){
+    $('.colapse-menu-button .c-hamburger').on('click',function(){
+      $('header .entry_block').slideToggle();
+    });
+  });
+  
+  // Hidden menu
+  var menu = $('header .entry_block'); 
+  $(window).on('resize',function(){
+    var wid = $(window).width();
+    if(wid > 576 && menu.is(':hidden')) {
+        menu.removeAttr('style');      
+    }
+  });
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    items:6,
     nav:true,
     responsive:{
         0:{
@@ -10,7 +43,8 @@ $('.owl-carousel').owlCarousel({
             items:3
         },
         1000:{
-            items:5
+            items:6
         }
     }
 })
+
